ba960c7cfa7196c5b9d7d66ab2bd77eb4ed6e6f1
Gulnaz Serikbay <gserikba@andrew.cmu.edu>
2022-06-09 09:45:35 +0600

    first

---
+++ script.js
@@ -0,0 +1,12 @@
+
+const video = document.getElementById("video-background")
+
+function playVideo () {
+    if (videoBackground.paused) {
+        videoBackground.play();
+    }
+    else {
+        videoBackground.pause();
+        
+    }
+}
